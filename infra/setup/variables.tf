73d876be9c43cbfc596bbe5257adba8b618e5fe5
Tom Debruyne <5540603+tdebruyn@users.noreply.github.com>
2025-03-25 11:30:32 +0100

    added permissions for network

--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -5,7 +5,7 @@ variable "tf_state_bucket" {
 
 variable "tf_state_lock_table" {
   description = "Name of the DynamoDB table for TF state locking"
-  default     = "devops-recipe-app-api-tf-state"
+  default     = "devops-recipe-app-api-tf-lock"
 }
 
 variable "project" {
