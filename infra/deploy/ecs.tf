567349b5b6085da36be4fb682cc8f0f006feb8f3
Tom Debruyne <5540603+tdebruyn@users.noreply.github.com>
2025-03-25 18:06:28 +0100

    added ecs cluster

---
+++ infra/deploy/ecs.tf
@@ -0,0 +1,8 @@
+##########################################
+# ECS Cluster for running app on Fargate #
+##########################################
+
+resource "aws_ecs_cluster" "main" {
+  name = "${local.prefix}-cluster"
+}
+
